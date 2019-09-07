6359455cea9f9db3967e0ac09f243db82b61ad31
kshitijrayamane <rayamanek@yahoo.com>
2019-09-07 15:46:11 +0530

    Commit After Some Changes

--- app/src/main/java/com/example/hospitalmanagementsys/Database.java
+++ app/src/main/java/com/example/hospitalmanagementsys/Database.java
@@ -61,6 +61,11 @@ public class Database extends SQLiteOpenHelper
         db.update(TABLE_NAME, contentValues, "ID = ?", new String[]{ id });
         return true;
     }
+    public Integer deleteData(String id)
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(TABLE_NAME, "ID = ? " , new String[]{id});
+    }
     public Cursor getAllData()
     {
       SQLiteDatabase  db = this.getWritableDatabase();
--- app/src/main/java/com/example/hospitalmanagementsys/Register.java
+++ app/src/main/java/com/example/hospitalmanagementsys/Register.java
@@ -1,17 +1,24 @@
 package com.example.hospitalmanagementsys;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+@RequiresApi(api = Build.VERSION_CODES.N)
 public class Register extends AppCompatActivity {
     Button button4; EditText editText,editText2,editText3,editText4,editText8,editText9,editText10;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,9 +57,37 @@ public class Register extends AppCompatActivity {
         return(!TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches());
     }
 
-
+    boolean isEmpty(EditText text)
+    {
+        CharSequence str = text.getText().toString();
+        return TextUtils.isEmpty(str);
+    }
     private void checkDataEntered()
     {
-
+        if(isEmpty(editText3))
+        {
+            Toast t = Toast.makeText(this, "You must enter first name to register!", Toast.LENGTH_SHORT);
+            t.show();
+        }
+        if(isEmpty(editText4))
+        {
+            editText4.setError("Address is Required!");
+        }
+        if(isEmpty(editText8))
+        {
+            editText8.setError("Enter Password");
+        }
+        if(isEmpty(editText10))
+        {
+            if(editText8.equals(editText9))
+            {
+                editText9.setError("Password Does Not Match");
+            }
+        }
+        if(isEmpty(editText))
+        {
+            editText.setError("Enter a Valid Mobile No");
+        }
     }
+    //Database.insertData(SQLiteDatabase db, int old, int newVer);
 }
